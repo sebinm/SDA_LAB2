268970ef61853861e08c1a2f390989048529b224
sebinm <92368335+sebinm@users.noreply.github.com>
2023-03-06 15:04:40 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <random>
+#include <chrono>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::sort;
+using std::swap;
+using std::setw;
+using std::setfill;
+
+#include "count.h"
+#include "nonCount.h"
+#include "AlgorithmTest.h"
+#include "GeneralPrint.h"
+
+int main(void)
+{
+	GeneralTime();
+
+	return 0;
+}
